6ac1452a00efdafc54191a7974f8de2fc61dc2c6
ms9849 <hee26368@naver.com>
2025-11-08 10:27:38 +0900

    디스토션 구현

--- Client/Private/Level_GamePlay.cpp
+++ Client/Private/Level_GamePlay.cpp
@@ -29,13 +29,13 @@ HRESULT CLevel_GamePlay::Initialize()
 	if (FAILED(Ready_Layer_Monster(TEXT("Layer_Monster"))))
 		return E_FAIL;
 
-	m_pGameInstance->ADD_DelayFunction(TEXT("Effect_Create"), 10.f, [&]()
-		{
-			Ready_Layer_Effect(TEXT("Layer_Effect"));
-		});
+	//m_pGameInstance->ADD_DelayFunction(TEXT("Effect_Create"), 10.f, [&]()
+	//	{
+	//		Ready_Layer_Effect(TEXT("Layer_Effect"));
+	//	});
 
-	//if (FAILED(Ready_Layer_Effect(TEXT("Layer_Effect"))))
-	//	return E_FAIL;
+	if (FAILED(Ready_Layer_Effect(TEXT("Layer_Effect"))))
+		return E_FAIL;
 
 	return S_OK;
 }
