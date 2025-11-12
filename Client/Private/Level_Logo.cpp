d8a098a2a5830ee072b76af5826bc769e2633d31
sunho-5810 <sunho5810@gmail.com>
2025-11-13 02:39:18 +0900

    Ready_Layer_Background 주석처리

--- Client/Private/Level_Logo.cpp
+++ Client/Private/Level_Logo.cpp
@@ -24,8 +24,8 @@ HRESULT CLevel_Logo::Initialize()
 	//if (FAILED(Ready_Layer_Camera(TEXT("Layer_Camera"))))
 	//	return E_FAIL;	
 
-	if (FAILED(Ready_Layer_BackGround(TEXT("Layer_BackGround"))))
-		return E_FAIL;
+	//if (FAILED(Ready_Layer_BackGround(TEXT("Layer_BackGround"))))
+	//	return E_FAIL;
 
 	if (FAILED(Ready_Layer_UI(TEXT("Layer_UI"))))
 		return E_FAIL;
