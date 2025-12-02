3157c6450b0794d2ee38e8e8af2f2f3173f2246a
IBulan <40731551+IBulan@users.noreply.github.com>
2025-12-02 11:44:46 +0900

    이펙트 주석 처리

--- Client/Private/Weapon.cpp
+++ Client/Private/Weapon.cpp
@@ -65,6 +65,27 @@ void CWeapon::Update(_float fTimeDelta)
 	//	m_pSpark->Stop();
 	//if (m_pGameInstance->KeyDown(KEY_INPUT::KEYBOARD, DIK_O))
 	//	m_pSpark->Play();
+	//if (nullptr != m_pCharge) {
+	//	m_pCharge->Update(fTimeDelta);
+	//	if (m_pGameInstance->KeyDown(KEY_INPUT::MOUSE, ENUM_CLASS(MOUSEKEYSTATE::RBUTTON)))
+	//		m_pCharge->Play();
+	//	if (m_pGameInstance->KeyUp(KEY_INPUT::MOUSE, ENUM_CLASS(MOUSEKEYSTATE::RBUTTON)))
+	//		m_pCharge->End();
+	//	if (m_pCharge->isDead())
+	//		Safe_Release(m_pCharge);
+	//}
+	//else if (m_pGameInstance->KeyDown(KEY_INPUT::MOUSE, ENUM_CLASS(MOUSEKEYSTATE::RBUTTON))) {
+	//	CEffect::EFFECT_TRANSFORM_DESC desc;
+	//	desc.fRotationPerSec = 1.f;
+	//	desc.fSpeedPerSec = 1.f;
+	//	desc.pRootMatrix = &m_CombinedWorldMatrix;
+	//	desc.vPos = XMVectorSet(0.06f, 0.f, 0, 1);
+	//	desc.fRot = _float3(0, 0, 0);
+	//	desc.fSize = 10.f;
+	//
+	//	m_pCharge = static_cast<CEffect*>(m_pGameInstance->Clone_Prototype(PROTOTYPE::GAMEOBJECT, ENUM_CLASS(LEVEL::GAMEPLAY), TEXT("Prototype_Component_Effect_Heal"), &desc));
+	//	m_pCharge->Play();
+	//}
 }
 
 void CWeapon::Late_Update(_float fTimeDelta)
@@ -83,7 +104,11 @@ void CWeapon::Late_Update(_float fTimeDelta)
 		m_pColliderCom->UpdateColiision(XMLoadFloat4x4(&m_CombinedWorldMatrix));
 		m_pGameInstance->ADD_Collider(m_pColliderCom);
 	}                      
-	//m_pTrail->Update_Trail(XMLoadFloat4x4(&m_CombinedWorldMatrix), fTimeDelta, true);
+	//m_pTrail->Update_Trail(XMLoadFloat4x4(&m_CombinedWorldMatrix), fTimeDelta, true);;
+	//m_pSpark->Late_Update(fTimeDelta);
+	//if (nullptr != m_pCharge) {
+	//	m_pCharge->Late_Update(fTimeDelta);
+	//}
 
 	m_pGameInstance->Add_RenderGroup(RENDER::SHADOW, this);
 	m_pGameInstance->Add_RenderGroup(RENDER::NONBLEND, this);
@@ -259,4 +284,5 @@ void CWeapon::Free()
 	Safe_Release(m_pColliderCom);
 	Safe_Release(m_pTrail);
 	Safe_Release(m_pSpark);
+	Safe_Release(m_pCharge);
 }
