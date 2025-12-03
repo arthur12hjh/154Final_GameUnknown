489b989bc9a0875043648c83f1050045041c675e
sunho-5810 <sunho5810@gmail.com>
2025-12-03 22:19:10 +0900

    러쉬 스킬 테스트 완료 베타 스킬 정상화 완료

--- Client/Private/UIBase.cpp
+++ Client/Private/UIBase.cpp
@@ -62,14 +62,12 @@ void CUIBase::Update(_float fTimeDelta)
 		//부모 따라가기
 		if (dynamic_cast<CUIBase*>(m_pParent))
 		{
-			m_eVisibility = m_pParent->GetVisibility();
 		
 			m_tUIDesc.fX = dynamic_cast<CUIBase*>(m_pParent)->Get_UIBase_Desc().fX + dynamic_cast<CUIBase*>(m_pParent)->Get_UIBase_Desc().fOffsetX;
 			m_tUIDesc.fY = dynamic_cast<CUIBase*>(m_pParent)->Get_UIBase_Desc().fY + dynamic_cast<CUIBase*>(m_pParent)->Get_UIBase_Desc().fOffsetY;
 		
+			//m_eVisibility = VISIBILITY(dynamic_cast<CUIBase*>(m_pParent)->Get_UIBase_Desc().iVisiblity);
 			//m_tUIDesc.iVisiblity = dynamic_cast<CUIBase*>(m_pParent)->Get_UIBase_Desc().iVisiblity;
-
-			m_eVisibility = m_pParent->GetVisibility();
 
 			/*if (dynamic_cast<CUIWorldWrapper*>(m_pParent) && m_pParent->GetVisibility() == VISIBILITY::VISIBLE)
 			{
@@ -349,7 +347,6 @@ void CUIBase::Update_Children(CUIBase* pObj)
 
 	for (auto& pChild : *pObj->Get_Children())
 	{
-		//if(pChild->Get_UIBase_Desc().szUITag != TEXT("MonsterHp_Fx"))
 		pObj->Update_Children(pChild);
 	}
 }
