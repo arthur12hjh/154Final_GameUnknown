c535b53ae515e106dcee19b92f1df9841526c775
한재훈 <76739123+arthur12hjh@users.noreply.github.com>
2025-12-08 13:30:59 +0900

    png 텍스쳐들 자동으로 동일경로 dds 탐색해서 변경하는 기능 제작

--- Engine/Private/Material.cpp
+++ Engine/Private/Material.cpp
@@ -37,6 +37,23 @@ HRESULT CMaterial::Initialize(const _char* pModelFilePath, const binMaterial* pB
 			_splitpath_s(pModelFilePath, szDrive, MAX_PATH, szDir, MAX_PATH, nullptr, 0, nullptr, 0);
 			_splitpath_s(strTexturePath, nullptr, 0, nullptr, 0, szFileName, MAX_PATH, szEXT, MAX_PATH);
 
+
+			if (false == strcmp(".png", szEXT))
+			{
+				char	strDDSTexturePath[MAX_PATH];
+				strcpy_s(strDDSTexturePath, strTexturePath);
+
+				strcpy_s(strDDSTexturePath, szDrive);
+				strcat_s(strDDSTexturePath, szDir);
+				strcat_s(strDDSTexturePath, szFileName);
+				strcat_s(strDDSTexturePath, ".dds");
+
+				if ((_access(strDDSTexturePath, 0) != -1))
+				{
+					strcpy_s(szEXT, ".dds");
+				}
+			}
+
 			strcpy_s(szTextureFilePath, szDrive);
 			strcat_s(szTextureFilePath, szDir);
 			strcat_s(szTextureFilePath, szFileName);
@@ -49,6 +66,7 @@ HRESULT CMaterial::Initialize(const _char* pModelFilePath, const binMaterial* pB
 			ID3D11ShaderResourceView* pSRV = { nullptr };
 
 			HRESULT			hr = {};
+
 
 			if (false == strcmp(".dds", szEXT))
 				hr = CreateDDSTextureFromFile(m_pDevice, szAbsolutePath, nullptr, &pSRV);
