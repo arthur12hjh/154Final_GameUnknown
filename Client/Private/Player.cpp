6cec8fc1a40bf55cc2b1ee240e616347d547251a
IBulan <40731551+IBulan@users.noreply.github.com>
2025-12-12 16:31:22 +0900

    데이터 파일 수정

--- Client/Private/Player.cpp
+++ Client/Private/Player.cpp
@@ -435,8 +435,8 @@ HRESULT CPlayer::Ready_PlayerDesc()
 	m_PlayerDesc.fCurrentLinkApplyDamage = 100.f;
 	m_PlayerDesc.iCurrentAttackPoint = 100;
 
-	m_PlayerDesc.iCurrentPotions = 3;
-	m_PlayerDesc.iMaxPotions = 3;
+	m_PlayerDesc.iCurrentPotions = 5;
+	m_PlayerDesc.iMaxPotions = 5;
 
 	m_PlayerDesc.eRushState = SKILL_STATE::DEFAULT;
 	m_PlayerDesc.fMaxRushCoolTime = 5.f;
