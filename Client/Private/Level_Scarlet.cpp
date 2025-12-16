7cb9f8803978d66cc4ef18130626eb7310edd52b
chanbingim <127704542+chanbingim@users.noreply.github.com>
2025-12-16 17:24:20 +0900

    슈정

--- Client/Private/Level_Scarlet.cpp
+++ Client/Private/Level_Scarlet.cpp
@@ -51,8 +51,8 @@ HRESULT CLevel_Scarlet::Initialize()
 	/*if (FAILED(Ready_Layer_Effect(TEXT("Layer_Effect"))))
 		return E_FAIL;*/
 
-	if (FAILED(Ready_Layer_UI(TEXT("Layer_UI"))))
-		return E_FAIL;
+	//if (FAILED(Ready_Layer_UI(TEXT("Layer_UI"))))
+	//	return E_FAIL;
 
 	Load_Map_Data();
 	auto pGameCharacter = CGameManager::GetInstance()->GetGameCharacter();
