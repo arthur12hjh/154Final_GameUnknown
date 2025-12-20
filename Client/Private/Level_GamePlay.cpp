8df2041c067bc70df0a4b4f339186362a39fe2cc
한재훈 <76739123+arthur12hjh@users.noreply.github.com>
2025-12-20 16:11:40 +0900

    매핑 레이턴시 적용(1프레임 지연), 버퍼 갱신방식 변경

--- Client/Private/Level_GamePlay.cpp
+++ Client/Private/Level_GamePlay.cpp
@@ -62,8 +62,8 @@ HRESULT CLevel_GamePlay::Initialize()
 
 	Load_Map_Desert_Data("../../Map_Editor/Bin/DataFiles/MapData_Desert2.bin");
 	//Load_Map_Desert_Data("../../Map_Editor/Bin/DataFiles/Test.bin");
-	//Load_Monster_Desert_Data("../../Map_Editor/Bin/DataFiles/MonsterData_Desert.bin");
-	//Load_Level_CinematicObjectData("../Bin/DataFiles/LevelCinematicObjectData/CinematicData_Desert.json");
+	Load_Monster_Desert_Data("../../Map_Editor/Bin/DataFiles/MonsterData_Desert.bin");
+	Load_Level_CinematicObjectData("../Bin/DataFiles/LevelCinematicObjectData/CinematicData_Desert.json");
 
 	auto pGameManager = CGameManager::GetInstance();
 	CAttackHitBox::HIT_BOX_DESC pHitBoxDesc = {};
