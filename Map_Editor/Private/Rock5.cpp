ade9f3fc9d4d37c0aea63be6e55d7713e8b5888a
JinDoL1998 <jkhgfdds1@gmail.com>
2025-12-27 17:44:00 +0900

    마을 제작중

--- Map_Editor/Private/Rock5.cpp
+++ Map_Editor/Private/Rock5.cpp
@@ -1,21 +1,19 @@
 #include "pch.h"
 #include "Rock5.h"
-
 #include "GameInstance.h"
 
-CRock5::CRock5(ID3D11Device* pDevice, ID3D11DeviceContext* pContext) :
-	CGameObject(pDevice, pContext)
+CRock5::CRock5(ID3D11Device* pDevice, ID3D11DeviceContext* pContext)
+	: CGameObject{ pDevice, pContext }
 {
 }
 
-CRock5::CRock5(const CRock5& Prototype) :
-	CGameObject(Prototype)
+CRock5::CRock5(const CRock5& Prototype)
+	: CGameObject{ Prototype }
 {
 }
 
 HRESULT CRock5::Initialize_Prototype()
 {
-
 	return S_OK;
 }
 
@@ -40,6 +38,7 @@ void CRock5::Update(_float fTimeDelta)
 
 void CRock5::Late_Update(_float fTimeDelta)
 {
+
 	m_pGameInstance->Add_RenderGroup(RENDER::NONBLEND, this);
 }
 
@@ -48,19 +47,21 @@ HRESULT CRock5::Render()
 	if (FAILED(Bind_ShaderResources()))
 		return E_FAIL;
 
-	_uint		iNumMeshes = m_pModelCom->GetModelNumMeshes();
+
+	_uint		iNumMeshes = m_pModelCom->Get_NumMeshes();
+
 	for (_uint i = 0; i < iNumMeshes; i++)
 	{
-		if (FAILED(m_pModelCom->Bind_MatrialTexture(m_pShaderCom, i, "g_DiffuseTexture", aiTextureType_DIFFUSE, 0)))
+
+		if (FAILED(m_pModelCom->Bind_Material(i, m_pShaderCom, "g_DiffuseTexture", aiTextureType_DIFFUSE, 0)))
+			return E_FAIL;
+		if (FAILED(m_pModelCom->Bind_Material(i, m_pShaderCom, "g_NormalTexture", aiTextureType_NORMALS, 0)))
 			return E_FAIL;
 
-		if (FAILED(m_pModelCom->Bind_MatrialTexture(m_pShaderCom, i, "g_NormalTexture", aiTextureType_NORMALS, 0)))
-			return E_FAIL;
-
-		
 
 		if (FAILED(m_pShaderCom->Begin(0)))
 			return E_FAIL;
+
 
 		if (FAILED(m_pModelCom->Render(i)))
 			return E_FAIL;
@@ -77,7 +78,7 @@ HRESULT CRock5::Ready_Components()
 		return E_FAIL;
 
 	/* Com_Shader */
-	if (FAILED(__super::Add_Component(ENUM_CLASS(LEVEL::VILLAGE), TEXT("Prototype_Component_Shader_Instance_Model"),
+	if (FAILED(__super::Add_Component(ENUM_CLASS(LEVEL::VILLAGE), TEXT("Prototype_Component_Shader_VtxMesh"),
 		TEXT("Com_Shader"), reinterpret_cast<CComponent**>(&m_pShaderCom))))
 		return E_FAIL;
 
@@ -86,6 +87,7 @@ HRESULT CRock5::Ready_Components()
 
 HRESULT CRock5::Bind_ShaderResources()
 {
+	/*m_pShaderCom->Bind_Matrix("g_WorldMatrix", );*/
 	if (FAILED(m_pTransformCom->Bind_ShaderResource(m_pShaderCom, "g_WorldMatrix")))
 		return E_FAIL;
 
@@ -99,24 +101,28 @@ HRESULT CRock5::Bind_ShaderResources()
 
 CRock5* CRock5::Create(ID3D11Device* pDevice, ID3D11DeviceContext* pContext)
 {
-	CRock5* pInstanceModel = new CRock5(pDevice, pContext);
-	if (FAILED(pInstanceModel->Initialize_Prototype()))
+	CRock5* pInstance = new CRock5(pDevice, pContext);
+
+	if (FAILED(pInstance->Initialize_Prototype()))
 	{
-		Safe_Release(pInstanceModel);
-		MSG_BOX("Create Fail : Instance Model");
+		MSG_BOX("Failed to Created : pGraphic_Device");
+		Safe_Release(pInstance);
 	}
-	return pInstanceModel;
+
+	return pInstance;
 }
 
 CGameObject* CRock5::Clone(void* pArg)
 {
-	CRock5* pInstanceModel = new CRock5(*this);
-	if (FAILED(pInstanceModel->Initialize(pArg)))
+	CRock5* pInstance = new CRock5(*this);
+
+	if (FAILED(pInstance->Initialize(pArg)))
 	{
-		Safe_Release(pInstanceModel);
-		MSG_BOX("Clone Fail : Instance Model");
+		MSG_BOX("Failed to Cloned : CRock5");
+		Safe_Release(pInstance);
 	}
-	return pInstanceModel;
+
+	return pInstance;
 }
 
 void CRock5::Free()
